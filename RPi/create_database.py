08f85f4164e346a480dac31cba26e2c46d0cd64d
Brandon Huang <hhe.brandon@berkeley.edu>
2016-03-08 22:34:05 -0800

    user prompt on create_databse

--- RPi/create_database.py
+++ RPi/create_database.py
@@ -19,3 +19,32 @@ x.execute("CREATE TABLE Env_variables(reset_value INT(11))")
 x.execute("INSERT INTO Env_variables(reset_value) VALUES ('0')")
 conn.commit()
 
+def getserial():
+    cpuserial= "0000000000000000"
+    try:
+        f = open('/proc/cpuinfo', 'r')
+        for line in f:
+            if line[0:6]=='Serial':
+                cpuserial = line[10:26]
+        f.close()
+    except:
+        cpuserial="ERROR00000000000"
+        raise
+    return cpuserial
+
+user_resp = raw_input("Change this RPi's mapping to the new database %s? y/n: " % db_name)
+
+if user_resp == 'y' or user_resp == 'Y':
+    conn = MySQLdb.connect(host="173.194.252.192", user="infinite-uptime", passwd="infiniteuptime", db="Hub_Dictionary")
+    x=conn.cursor()
+    x.execute("SHOW DATABASES")
+    db_names = x.fetchall()
+    db_names = zip(*db_names)[0]
+    print db_names
+    print db_name
+    print getserial()
+    if db_name not in db_names:
+        raise Exception("You must first create that database.")
+    x.execute("DELETE FROM Hub_Mapping WHERE Hub_ID='%s'" % getserial())
+    x.execute("INSERT INTO Hub_Mapping (Hub_ID, Database_Name) VALUES (%s, %s)", (getserial(), db_name))
+    conn.commit()
