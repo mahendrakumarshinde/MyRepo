b95c8884c06ccef9d0f84f86d2e22a4949bafd55
bapatzankar <bapatzankar@gmail.com>
2016-10-12 15:26:37 -0700

    WiFi module firmware basic code with accelerometer readings and cloud upload

---
+++ wifi_firmware_main_with_accel/constants.h
@@ -0,0 +1,218 @@
+//======================= Register Addresses and Enums =========================
+
+// See MS5637-02BA03 Low Voltage Barometric Pressure Sensor Data Sheet http://www.meas-spec.com/downloads/MS5637-02BA03.pdf
+#define MS5637_RESET      0x1E
+#define MS5637_CONVERT_D1 0x40
+#define MS5637_CONVERT_D2 0x50
+#define MS5637_ADC_READ   0x00
+
+// MPU-9250 accel register constants
+#define SELF_TEST_X_ACCEL 0x0D
+#define SELF_TEST_Y_ACCEL 0x0E
+#define SELF_TEST_Z_ACCEL 0x0F
+
+#define SMPLRT_DIV       0x19
+#define CONFIG           0x1A
+#define GYRO_CONFIG      0x1B
+#define ACCEL_CONFIG     0x1C
+#define ACCEL_CONFIG2    0x1D
+#define LP_ACCEL_ODR     0x1E
+#define WOM_THR          0x1F
+
+#define MOT_DUR          0x20  // Duration counter threshold for motion interrupt generation, 1 kHz rate, LSB = 1 ms
+#define ZMOT_THR         0x21  // Zero-motion detection threshold bits [7:0]
+#define ZRMOT_DUR        0x22  // Duration counter threshold for zero motion interrupt generation, 16 Hz rate, LSB = 64 ms
+
+#define FIFO_EN          0x23
+#define I2C_MST_CTRL     0x24
+#define I2C_SLV0_ADDR    0x25
+#define I2C_SLV0_REG     0x26
+#define I2C_SLV0_CTRL    0x27
+#define I2C_SLV1_ADDR    0x28
+#define I2C_SLV1_REG     0x29
+#define I2C_SLV1_CTRL    0x2A
+#define I2C_SLV2_ADDR    0x2B
+#define I2C_SLV2_REG     0x2C
+#define I2C_SLV2_CTRL    0x2D
+#define I2C_SLV3_ADDR    0x2E
+#define I2C_SLV3_REG     0x2F
+#define I2C_SLV3_CTRL    0x30
+#define I2C_SLV4_ADDR    0x31
+#define I2C_SLV4_REG     0x32
+#define I2C_SLV4_DO      0x33
+#define I2C_SLV4_CTRL    0x34
+#define I2C_SLV4_DI      0x35
+#define I2C_MST_STATUS   0x36
+#define INT_PIN_CFG      0x37
+#define INT_ENABLE       0x38
+#define DMP_INT_STATUS   0x39  // Check DMP interrupt
+#define INT_STATUS       0x3A
+#define ACCEL_XOUT_H     0x3B
+#define ACCEL_XOUT_L     0x3C
+#define ACCEL_YOUT_H     0x3D
+#define ACCEL_YOUT_L     0x3E
+#define ACCEL_ZOUT_H     0x3F
+#define ACCEL_ZOUT_L     0x40
+#define MOT_DETECT_STATUS 0x61
+#define I2C_SLV0_DO      0x63
+#define I2C_SLV1_DO      0x64
+#define I2C_SLV2_DO      0x65
+#define I2C_SLV3_DO      0x66
+#define I2C_MST_DELAY_CTRL 0x67
+#define SIGNAL_PATH_RESET  0x68
+#define MOT_DETECT_CTRL  0x69
+#define USER_CTRL        0x6A  // Bit 7 enable DMP, bit 3 reset DMP
+#define PWR_MGMT_1       0x6B // Device defaults to the SLEEP mode
+#define PWR_MGMT_2       0x6C
+#define DMP_BANK         0x6D  // Activates a specific bank in the DMP
+#define DMP_RW_PNT       0x6E  // Set read/write pointer to a specific start address in specified DMP bank
+#define DMP_REG          0x6F  // Register in DMP from which to read or to which to write
+#define DMP_REG_1        0x70
+#define DMP_REG_2        0x71
+#define FIFO_COUNTH      0x72
+#define FIFO_COUNTL      0x73
+#define FIFO_R_W         0x74
+#define WHO_AM_I_MPU9250 0x75 // Should return 0x71
+#define XA_OFFSET_H      0x77
+#define XA_OFFSET_L      0x78
+#define YA_OFFSET_H      0x7A
+#define YA_OFFSET_L      0x7B
+#define ZA_OFFSET_H      0x7D
+#define ZA_OFFSET_L      0x7E
+
+#define MPU9250_ADDRESS 0x68  // Device address when ADO = 0
+
+
+// EM7180 SENtral register map
+// see http://www.emdeveloper.com/downloads/7180/EMSentral_EM7180_Register_Map_v1_3.pdf
+//
+#define EM7180_QX                 0x00  // this is a 32-bit normalized floating point number read from registers 0x00-03
+#define EM7180_QY                 0x04  // this is a 32-bit normalized floating point number read from registers 0x04-07
+#define EM7180_QZ                 0x08  // this is a 32-bit normalized floating point number read from registers 0x08-0B
+#define EM7180_QW                 0x0C  // this is a 32-bit normalized floating point number read from registers 0x0C-0F
+#define EM7180_QTIME              0x10  // this is a 16-bit unsigned integer read from registers 0x10-11
+#define EM7180_MX                 0x12  // int16_t from registers 0x12-13
+#define EM7180_MY                 0x14  // int16_t from registers 0x14-15
+#define EM7180_MZ                 0x16  // int16_t from registers 0x16-17
+#define EM7180_MTIME              0x18  // uint16_t from registers 0x18-19
+#define EM7180_AX                 0x1A  // int16_t from registers 0x1A-1B
+#define EM7180_AY                 0x1C  // int16_t from registers 0x1C-1D
+#define EM7180_AZ                 0x1E  // int16_t from registers 0x1E-1F
+#define EM7180_ATIME              0x20  // uint16_t from registers 0x20-21
+#define EM7180_GX                 0x22  // int16_t from registers 0x22-23
+#define EM7180_GY                 0x24  // int16_t from registers 0x24-25
+#define EM7180_GZ                 0x26  // int16_t from registers 0x26-27
+#define EM7180_GTIME              0x28  // uint16_t from registers 0x28-29
+#define EM7180_QRateDivisor       0x32  // uint8_t
+#define EM7180_EnableEvents       0x33
+#define EM7180_HostControl        0x34
+#define EM7180_EventStatus        0x35
+#define EM7180_SensorStatus       0x36
+#define EM7180_SentralStatus      0x37
+#define EM7180_AlgorithmStatus    0x38
+#define EM7180_FeatureFlags       0x39
+#define EM7180_ParamAcknowledge   0x3A
+#define EM7180_SavedParamByte0    0x3B
+#define EM7180_SavedParamByte1    0x3C
+#define EM7180_SavedParamByte2    0x3D
+#define EM7180_SavedParamByte3    0x3E
+#define EM7180_ActualMagRate      0x45
+#define EM7180_ActualAccelRate    0x46
+#define EM7180_ActualGyroRate     0x47
+#define EM7180_ErrorRegister      0x50
+#define EM7180_AlgorithmControl   0x54
+#define EM7180_MagRate            0x55
+#define EM7180_AccelRate          0x56
+#define EM7180_GyroRate           0x57
+#define EM7180_LoadParamByte0     0x60
+#define EM7180_LoadParamByte1     0x61
+#define EM7180_LoadParamByte2     0x62
+#define EM7180_LoadParamByte3     0x63
+#define EM7180_ParamRequest       0x64
+#define EM7180_ROMVersion1        0x70
+#define EM7180_ROMVersion2        0x71
+#define EM7180_RAMVersion1        0x72
+#define EM7180_RAMVersion2        0x73
+#define EM7180_ProductID          0x90
+#define EM7180_RevisionID         0x91
+#define EM7180_RunStatus          0x92
+#define EM7180_UploadAddress      0x94 // uint16_t registers 0x94 (MSB)-5(LSB)
+#define EM7180_UploadData         0x96
+#define EM7180_CRCHost            0x97  // uint32_t from registers 0x97-9A
+#define EM7180_ResetRequest       0x9B
+#define EM7180_PassThruStatus     0x9E
+#define EM7180_PassThruControl    0xA0
+
+
+#define MS5637_ADDRESS      0x76   // Address of MS5637 altimeter
+#define EM7180_ADDRESS      0x28   // Address of the EM7180 SENtral sensor hub
+#define M24512DFM_DATA_ADDRESS   0x50   // Address of the 500 page M24512DFM EEPROM data buffer, 1024 bits (128 8-bit bytes) per page
+#define M24512DFM_IDPAGE_ADDRESS 0x58   // Address of the single M24512DFM lockable EEPROM ID page
+
+// Set initial input parameters
+// define MPU ACC full scale options
+// CHANGE: new constants for MPU
+#define AFS_2G  0x0
+#define AFS_4G  0x1
+#define AFS_8G  0x2
+#define AFS_16G 0x3
+
+
+enum ACCBW {    // define BMX055 accelerometer bandwidths
+  ABW_8Hz,      // 7.81 Hz,  64 ms update time
+  ABW_16Hz,     // 15.63 Hz, 32 ms update time
+  ABW_31Hz,     // 31.25 Hz, 16 ms update time
+  ABW_63Hz,     // 62.5  Hz,  8 ms update time
+  ABW_125Hz,    // 125   Hz,  4 ms update time
+  ABW_250Hz,    // 250   Hz,  2 ms update time
+  ABW_500Hz,    // 500   Hz,  1 ms update time
+  ABW_1000Hz     // 1000  Hz,  0.5 ms update time
+};
+
+enum Gscale {
+  GFS_2000DPS = 0,
+  GFS_1000DPS,
+  GFS_500DPS,
+  GFS_250DPS,
+  GFS_125DPS
+};
+
+enum GODRBW {
+  G_2000Hz523Hz = 0, // 2000 Hz ODR and unfiltered (bandwidth 523Hz)
+  G_2000Hz230Hz,
+  G_1000Hz116Hz,
+  G_400Hz47Hz,
+  G_200Hz23Hz,
+  G_100Hz12Hz,
+  G_200Hz64Hz,
+  G_100Hz32Hz  // 100 Hz ODR and 32 Hz bandwidth
+};
+
+enum MODR {
+  MODR_10Hz = 0,   // 10 Hz ODR
+  MODR_2Hz     ,   // 2 Hz ODR
+  MODR_6Hz     ,   // 6 Hz ODR
+  MODR_8Hz     ,   // 8 Hz ODR
+  MODR_15Hz    ,   // 15 Hz ODR
+  MODR_20Hz    ,   // 20 Hz ODR
+  MODR_25Hz    ,   // 25 Hz ODR
+  MODR_30Hz        // 30 Hz ODR
+};
+
+enum Mmode {
+  lowPower         = 0,   // rms noise ~1.0 microTesla, 0.17 mA power
+  Regular             ,   // rms noise ~0.6 microTesla, 0.5 mA power
+  enhancedRegular     ,   // rms noise ~0.5 microTesla, 0.8 mA power
+  highAccuracy            // rms noise ~0.3 microTesla, 4.9 mA power
+};
+
+// MS5637 pressure sensor sample rates
+#define ADC_256  0x00 // define pressure and temperature conversion rates
+#define ADC_512  0x02
+#define ADC_1024 0x04
+#define ADC_2048 0x06
+#define ADC_4096 0x08
+#define ADC_8192 0x0A
+#define ADC_D1   0x40
+#define ADC_D2   0x50
+
