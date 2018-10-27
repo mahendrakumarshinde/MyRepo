5aebe3eb144508da5f2ae2eb1e53f4e58be377c4
Vikas Gaikwad <vksgaikwad3@gmail.com>
2018-10-27 17:09:12 +0530

    updated the Wifi Firmware version to 1.0.1

--- 1. IDE_PLUS Firmwares/wifi_station_ESP8285/BoardDefinition.h
+++ 1. IDE_PLUS Firmwares/wifi_station_ESP8285/BoardDefinition.h
@@ -9,7 +9,7 @@
 ============================================================================= */
 
 /***** Firmware version *****/
-const char FIRMWARE_VERSION[6] = "1.0.0";
+const char FIRMWARE_VERSION[6] = "1.0.1";
 
 
 /***** Device Type *****/
