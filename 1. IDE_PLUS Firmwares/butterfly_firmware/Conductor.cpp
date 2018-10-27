50fff05f4fa176ce57151233f1f8313c791ecd5a
Vikas Gaikwad <vksgaikwad3@gmail.com>
2018-10-27 18:32:43 +0530

    included case to handle MSP command to ASK FIRMWARE VERSION of HOST

--- 1. IDE_PLUS Firmwares/butterfly_firmware/Conductor.cpp
+++ 1. IDE_PLUS Firmwares/butterfly_firmware/Conductor.cpp
@@ -711,6 +711,10 @@ void Conductor::processWiFiMessage(IUSerial *iuSerial)
             if (loopDebugMode) { debugPrint(F("ASK_BLE_MAC")); }
             iuWiFi.sendBleMacAddress(m_macAddress);
             break;
+	case MSPCommand::ASK_HOST_FIRMWARE_VERSION:
+            if(loopDebugMode){ debugPrint(F("ASK_HOST_FIRMWARE_VERSION")); }
+            iuWiFi.sendHostFirmwareVersion(FIRMWARE_VERSION);               
+            break;
         case MSPCommand::WIFI_ALERT_CONNECTED:
             if (loopDebugMode) { debugPrint(F("WIFI-CONNECTED;")); }
             if (isBLEConnected()) {
