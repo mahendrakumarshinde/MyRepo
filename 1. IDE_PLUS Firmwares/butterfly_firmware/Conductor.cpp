28d42d423f3b413a00605f20a22bea8b40d9b1e7
Mahendra Shinde <mahendrakumar@infinite-uptime.com>
2020-09-17 11:40:03 +0530

    Sent Fingerprints to BLE app

--- 1. IDE_PLUS Firmwares/butterfly_firmware/Conductor.cpp
+++ 1. IDE_PLUS Firmwares/butterfly_firmware/Conductor.cpp
@@ -4556,7 +4556,12 @@ void Conductor::sendDiagnosticFingerPrints() {
                     { 
                         iuWiFi.sendMSPCommand(MSPCommand::SEND_DIAGNOSTIC_RESULTS,FingerPrintResult );    
                     }
+                    if(StreamingMode::BLE && isBLEConnected()){    
+                    iuBluetooth.write("Fingerprints Data : ");
+                    iuBluetooth.write(FingerPrintResult);
+        }
                 }
+                    
                 
             }
             else { // not published as time_diff < 500 ms
