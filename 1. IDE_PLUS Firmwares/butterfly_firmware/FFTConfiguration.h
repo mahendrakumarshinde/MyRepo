5ae67f8eb3d5191297977b57da4fa1b124dbdcbd
vikas <vikas@infinite-uptime.com>
2020-12-08 17:01:28 +0530

    Changed the default Lower cutoff frequency of LSM to 10Hz

--- 1. IDE_PLUS Firmwares/butterfly_firmware/FFTConfiguration.h
+++ 1. IDE_PLUS Firmwares/butterfly_firmware/FFTConfiguration.h
@@ -37,7 +37,7 @@ namespace FFTConfiguration {
     // Default parameter values
     const int DEFAULT_SAMPLING_RATE = 25600;
     const int DEFALUT_LOW_CUT_OFF_FREQUENCY_KNX = 10;
-    const int DEFALUT_LOW_CUT_OFF_FREQUENCY_LSM = 5;
+    const int DEFALUT_LOW_CUT_OFF_FREQUENCY_LSM = 10;
     const int DEFAULT_BLOCK_SIZE = 8192;
     const int DEFALUT_LOW_CUT_OFF_FREQUENCY = 5;
     const int DEFAULT_HIGH_CUT_OFF_FREQUENCY = DEFAULT_SAMPLING_RATE / FMAX_FACTOR;  
